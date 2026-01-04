4ea1fc7d1bcd53f193d18332a139e6648f919f99
ludoc <ludoc.dev@gmail.com>
2026-01-04 14:10:29 -0300

    Fix base path case

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/O-segredo-por-tras-dos-segredos/',
+    base: '/o-segredo-por-tras-dos-segredos/',
     server: {
       port: 3000,
       host: '0.0.0.0',
